b9a0b933f89c12fcd24310ee9cabfd16c1335ad9
It5Me <56817602+It5Me@users.noreply.github.com>
2021-07-19 23:39:28 +0700

    signup

--- src/components/screens/SignupScreen.js
+++ src/components/screens/SignupScreen.js
@@ -3,15 +3,97 @@ import axios from 'axios';
 import { Link } from 'react-route-dom';
 import './Signup.css';
 
-const SignupScreen = () => {
+const SignupScreen = ({ history }) => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSignup = (e) => {
+        e.preventDefault();
+
+        const config = {
+            header: {
+                'Content-Type': 'application/json',
+            },
+        };
+
+        if (password !== confirmPassword) {
+            setPassword = '';
+            setConfirmPassword = '';
+            setTimeout(() => {
+                setError('');
+            }, 5000);
+            return setError('Password not match');
+        }
+        try {
+            const { data } = await axios.post(
+                'signup',
+                { username, email, password },
+                config()
+            );
+
+            localStorage.setItem('authToken', data.token);
+            history.push('/');
+        } catch (err) {
+            setError(err.response.data.error);
+            setTimeout(() => {
+                setError('');
+            }, 5000);
+        }
+    };
     return (
-        <div className='register-screen'>
-            <form className='register-screen__form'>
-                <h3 className='register-screen__title'>Register</h3>
+        <div className='signup-screen'>
+            <form onSubmit={handleSignup} className='signup-screen__form'>
+                <h3 className='signup-screen__title'>Signup</h3>
+                {error && <span className='error-message'>{error}</span>}
                 <div className='form-group'>
-                    <label htmlFor='username'></label>
+                    <label htmlFor='username'>Username: </label>
+                    <input
+                        type='text'
+                        require
+                        id='name'
+                        placeholder='Please enter username'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='email'>Email: </label>
+                    <input
+                        type='email'
+                        require
+                        id='email'
+                        placeholder='Please enter email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='password'>Password: </label>
+                    <input
+                        type='password'
+                        require
+                        id='password'
+                        placeholder='Please enter password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='confirmPassword'>ConfirmPassword: </label>
+                    <input
+                        type='confirmPassword'
+                        require
+                        id='confirmPassword'
+                        placeholder='Please enter confirmPassword'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
                 </div>
             </form>
         </div>
     );
 };
+export default SignupScreen;
